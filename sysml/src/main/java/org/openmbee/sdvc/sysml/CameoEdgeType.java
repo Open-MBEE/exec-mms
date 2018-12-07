d5c68b8c5c7f404030fbdf49c0d773a59b6ea10f
Lam, Doris T (393A) <Doris.T.Lam@jpl.nasa.gov>
2018-12-06 16:01:28 -0800

    cameo constants

---
+++ sysml/src/main/java/org/openmbee/sdvc/sysml/CameoEdgeType.java
@@ -0,0 +1,34 @@
+package org.openmbee.sdvc.sysml;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum CameoEdgeType {
+
+    CONTAINMENT(1),
+    VIEW(2),
+    TRANSCLUSION(3),
+    CHILDVIEW(4);
+
+    private static Map<Integer, CameoEdgeType> map = new HashMap<>();
+
+    static {
+        for (CameoEdgeType e : CameoEdgeType.values()) {
+            map.put(e.value, e);
+        }
+    }
+
+    private int value;
+
+    CameoEdgeType(int value) {
+        this.value = value;
+    }
+
+    public static CameoEdgeType valueOf(int e) {
+        return map.get(e);
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
