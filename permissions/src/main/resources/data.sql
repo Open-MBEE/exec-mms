64b7d42bef4db215ebd7a475355de4d3bdac065b
Jason Han <jason.han@jpl.nasa.gov>
2019-09-23 16:54:36 -0700

    Move dependency to example module and update privileges

--- permissions/src/main/resources/data.sql
+++ permissions/src/main/resources/data.sql
@@ -2,19 +2,22 @@ INSERT INTO privileges (id, modified, created, name) VALUES
   (1, NOW(), NOW(), 'ORG_READ'),
   (2, NOW(), NOW(), 'ORG_EDIT'),
   (3, NOW(), NOW(), 'ORG_UPDATE_PERMISSIONS'),
-  (4, NOW(), NOW(), 'ORG_CREATE_PROJECT'),
-  (5, NOW(), NOW(), 'ORG_DELETE'),
-  (6, NOW(), NOW(), 'PROJECT_READ'),
-  (7, NOW(), NOW(), 'PROJECT_EDIT'),
-  (8, NOW(), NOW(), 'PROJECT_READ_COMMITS'),
-  (9, NOW(), NOW(), 'PROJECT_CREATE_BRANCH'),
-  (10, NOW(), NOW(), 'PROJECT_DELETE'),
-  (11, NOW(), NOW(), 'PROJECT_UPDATE_PERMISSIONS'),
-  (12, NOW(), NOW(), 'PROJECT_CREATE_WEBHOOKS'),
-  (13, NOW(), NOW(), 'BRANCH_READ'),
-  (14, NOW(), NOW(), 'BRANCH_EDIT_CONTENT'),
-  (15, NOW(), NOW(), 'BRANCH_DELETE'),
-  (16, NOW(), NOW(), 'BRANCH_UPDATE_PERMISSIONS');
+  (4, NOW(), NOW(), 'ORG_READ_PERMISSIONS'),
+  (5, NOW(), NOW(), 'ORG_CREATE_PROJECT'),
+  (6, NOW(), NOW(), 'ORG_DELETE'),
+  (7, NOW(), NOW(), 'PROJECT_READ'),
+  (8, NOW(), NOW(), 'PROJECT_EDIT'),
+  (9, NOW(), NOW(), 'PROJECT_READ_COMMITS'),
+  (10, NOW(), NOW(), 'PROJECT_CREATE_BRANCH'),
+  (11, NOW(), NOW(), 'PROJECT_DELETE'),
+  (12, NOW(), NOW(), 'PROJECT_UPDATE_PERMISSIONS'),
+  (13, NOW(), NOW(), 'PROJECT_READ_PERMISSIONS'),
+  (14, NOW(), NOW(), 'PROJECT_CREATE_WEBHOOKS'),
+  (15, NOW(), NOW(), 'BRANCH_READ'),
+  (16, NOW(), NOW(), 'BRANCH_EDIT_CONTENT'),
+  (17, NOW(), NOW(), 'BRANCH_DELETE'),
+  (18, NOW(), NOW(), 'BRANCH_UPDATE_PERMISSIONS'),
+  (19, NOW(), NOW(), 'BRANCH_READ_PERMISSIONS');
 
 INSERT INTO roles (id, modified, created, name) VALUES
   (0, NOW(), NOW(), 'admin');
@@ -35,4 +38,7 @@ INSERT INTO roles_privileges (role_id, privilege_id) VALUES
   (0, 13),
   (0, 14),
   (0, 15),
-  (0, 16);
+  (0, 16),
+  (0, 17),
+  (0, 18),
+  (0, 19);
