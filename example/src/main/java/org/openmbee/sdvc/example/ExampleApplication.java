fd375012987c27a10e3cf187b7be827b4f23647b
Lam, Doris T (319E) <Doris.T.Lam@jpl.nasa.gov>
2019-10-04 16:46:15 -0700

    add auth to swagger ui

--- example/src/main/java/org/openmbee/sdvc/example/ExampleApplication.java
+++ example/src/main/java/org/openmbee/sdvc/example/ExampleApplication.java
@@ -1,14 +1,22 @@
 package org.openmbee.sdvc.example;
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -22,11 +30,23 @@ public class ExampleApplication {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+        return new Docket(DocumentationType.SWAGGER_2)
+            .ignoredParameterTypes(Authentication.class)
+            .apiInfo(apiInfo()).select()
             .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any()).build();
+            .paths(PathSelectors.any()).build()
+            .securitySchemes(Arrays.asList(
+                new BasicAuth("basicAuth"),
+                new ApiKey("bearerToken", "Authorization", "header")))
+            .securityContexts(Arrays.asList(
+                SecurityContext.builder().securityReferences(Arrays.asList(
+                    new SecurityReference("basicAuth", new AuthorizationScope[0]),
+                    new SecurityReference("bearerToken", new AuthorizationScope[0])
+                ))
+                .forPaths(PathSelectors.any()).build()
+            ));
     }
-
+    
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("MMS Example API")
             .description("Documentation for MMS Example API").termsOfServiceUrl("")
