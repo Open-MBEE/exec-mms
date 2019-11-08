58692a50ed240e14d1fb9207bba44dff0b117785
Jason Han <jason.han@jpl.nasa.gov>
2019-11-07 17:02:11 -0800

    Remove more moved classes

--- data/src/main/java/org/openmbee/sdvc/data/domains/global/Webhook.java
+++ data/src/main/java/org/openmbee/sdvc/data/domains/global/Webhook.java
@@ -6,7 +6,7 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "events",
+@Table(name = "webhooks",
     uniqueConstraints = {
         @UniqueConstraint(columnNames = {"uri","project_id"})
     })
