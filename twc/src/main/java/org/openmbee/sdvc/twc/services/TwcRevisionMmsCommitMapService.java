cd3b83de8e37739368a19d47d1988bf57ae1a140
Ward Schweitzer <ward.a.schweitzer@boeing.com>
2020-06-12 11:54:15 -0400

    Added ability to tget TWC revisions associated with commits of a branch

--- twc/src/main/java/org/openmbee/sdvc/twc/services/TwcRevisionMmsCommitMapService.java
+++ twc/src/main/java/org/openmbee/sdvc/twc/services/TwcRevisionMmsCommitMapService.java
@@ -1,19 +1,41 @@
 package org.openmbee.sdvc.twc.services;
 
 import org.openmbee.sdvc.core.config.ContextHolder;
+import org.openmbee.sdvc.core.dao.BranchDAO;
 import org.openmbee.sdvc.core.exceptions.InternalErrorException;
 import org.openmbee.sdvc.core.exceptions.NotFoundException;
 import org.openmbee.sdvc.core.objects.CommitsResponse;
 import org.openmbee.sdvc.core.services.NodeService;
 import org.openmbee.sdvc.crud.services.DefaultNodeService;
+import org.openmbee.sdvc.data.domains.scoped.Branch;
+import org.openmbee.sdvc.data.domains.scoped.Commit;
 import org.openmbee.sdvc.json.CommitJson;
 import org.openmbee.sdvc.twc.constants.TwcConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.ArrayList;
 
 @Service("twcRevisionMmsCommitMapService")
 public class TwcRevisionMmsCommitMapService extends DefaultNodeService implements NodeService {
+    private BranchDAO branchRepository;
+
+    @Autowired
+    public void setBranchRepository(BranchDAO branchRepository) {
+        this.branchRepository = branchRepository;
+    }
+
+    /**
+     * Method to update the twc revision id with commit id
+     * @param projectId
+     * @param commitId
+     * @param revisionId
+     * @return
+     */
     public CommitsResponse updateTwcRevisionID(String projectId, String commitId, String revisionId) {
         CommitsResponse commitsResponse = new CommitsResponse();
         if (revisionId.isEmpty() || revisionId.isBlank()) {
@@ -34,5 +56,39 @@ public class TwcRevisionMmsCommitMapService extends DefaultNodeService implement
             throw new InternalErrorException(exception);
         }
         return commitsResponse;
+    }
+
+    /**
+     * Method to get the twc revision list with project and branch id
+     * @param projectId
+     * @param refId
+     * @return
+     */
+    public List<CommitJson> getTwcRevisionList(String projectId, String refId) {
+        List<CommitJson> commits = new ArrayList<>();
+        ContextHolder.setContext(projectId);
+        Optional<Branch> ref = branchRepository.findByBranchId(refId);
+        if (!ref.isPresent()) {
+            throw new NotFoundException("Branch not found");
+        }
+        try {
+            List<Commit> refCommits = commitRepository.findByRefAndTimestampAndLimit(ref.get(), null, 0);
+            Set<String> commitIds = new HashSet<>();
+            refCommits.stream().forEach(commit -> {
+                commitIds.add(commit.getDocId());
+            });
+            List<CommitJson> commitJsonList = commitIndex.findAllById(commitIds);
+            if (null != commitJsonList && commitJsonList.size() > 0) {
+                commitJsonList.stream().forEach(commitJsonData -> {
+                    if (commitJsonData.containsKey(TwcConstants.TWCREVISIONID)) {
+                        commits.add(commitJsonData);
+                    }
+                });
+            }
+        } catch (Exception exception) {
+            logger.error("Error occurred while getting Twc Revision", exception.getMessage());
+            throw new InternalErrorException(exception);
+        }
+        return commits;
     }
 }
--- twc/src/main/java/org/openmbee/sdvc/twc/twcrevisionmmscommitmap/TwcRevisionMmsCommitMapController.java
+++ twc/src/main/java/org/openmbee/sdvc/twc/twcrevisionmmscommitmap/TwcRevisionMmsCommitMapController.java
@@ -2,13 +2,20 @@ package org.openmbee.sdvc.twc.twcrevisionmmscommitmap;
 
 import org.openmbee.sdvc.core.objects.CommitsResponse;
 import org.openmbee.sdvc.crud.controllers.BaseController;
+import org.openmbee.sdvc.json.CommitJson;
 import org.openmbee.sdvc.twc.services.TwcRevisionMmsCommitMapService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/projects/{projectId}/commits/{commitId}")
+@RequestMapping("/projects/{projectId}")
 public class TwcRevisionMmsCommitMapController extends BaseController {
 
     private TwcRevisionMmsCommitMapService twcRevisionMmsCommitMapService;
@@ -18,7 +25,7 @@ public class TwcRevisionMmsCommitMapController extends BaseController {
         this.twcRevisionMmsCommitMapService = twcRevisionMmsCommitMapService;
     }
 
-    @PutMapping(value = "/twc-revision/{revisionId}")
+    @PutMapping(value = "/commits/{commitId}/twc-revision/{revisionId}")
     @PreAuthorize("@mss.hasProjectPrivilege(authentication, #projectId, 'PROJECT_EDIT', true)")
     public CommitsResponse updateTwcRevisionID(
         @PathVariable String projectId,
@@ -26,4 +33,13 @@ public class TwcRevisionMmsCommitMapController extends BaseController {
         @PathVariable String revisionId) {
         return twcRevisionMmsCommitMapService.updateTwcRevisionID(projectId, commitId, revisionId);
     }
+
+    @GetMapping(value = "/refs/{refId}/twc-revisions")
+    @PreAuthorize("@mss.hasProjectPrivilege(authentication, #projectId, 'PROJECT_READ', true)")
+    public List<CommitJson> getTwcRevisionList(
+        @PathVariable String projectId,
+        @PathVariable String refId) {
+        return twcRevisionMmsCommitMapService.getTwcRevisionList(projectId, refId);
+    }
+
 }
