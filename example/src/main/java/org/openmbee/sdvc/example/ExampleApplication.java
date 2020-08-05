297f54ec826450fc2e6d0bb099552f5acd97f195
Jennifer Hang <jhang422@gmail.com>
2020-08-05 14:20:04 -0700

    Added CommonsRequestLoggingFilter

--- example/src/main/java/org/openmbee/sdvc/example/ExampleApplication.java
+++ example/src/main/java/org/openmbee/sdvc/example/ExampleApplication.java
@@ -10,6 +10,7 @@ import org.openmbee.sdvc.artifacts.storage.ArtifactStorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication(scanBasePackages = "org.openmbee")
 @OpenAPIDefinition(
@@ -38,7 +39,17 @@ public class ExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
     }
-
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+            = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
     //TODO: remove. This is just for testing until the real open source implementation is done.
     @Bean
     public ArtifactStorage getArtifactStorage() {
