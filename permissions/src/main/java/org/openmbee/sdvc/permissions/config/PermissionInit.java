dc0e89fb84d390da7537af59e45b3f2f6090e6ef
Jennifer Hang <jhang422@gmail.com>
2020-08-12 10:48:15 -0700

    Moved RPmap building code to Constants in core and added setter functions for private fields

--- permissions/src/main/java/org/openmbee/sdvc/permissions/config/PermissionInit.java
+++ permissions/src/main/java/org/openmbee/sdvc/permissions/config/PermissionInit.java
@@ -1,7 +1,5 @@
 package org.openmbee.sdvc.permissions.config;
 
-import org.openmbee.sdvc.core.config.Roles;
-import org.openmbee.sdvc.core.config.Privileges;
 import org.openmbee.sdvc.data.domains.global.Group;
 import org.openmbee.sdvc.data.domains.global.Privilege;
 import org.openmbee.sdvc.data.domains.global.Role;
@@ -12,32 +10,36 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import javax.transaction.Transactional;
 import java.util.*;
+import static org.openmbee.sdvc.core.config.Constants.RPmap;
+import static org.openmbee.sdvc.core.config.Constants.aPriv;
 
 @Component
 @Transactional
 public class PermissionInit implements ApplicationListener<ApplicationReadyEvent> {
-    @Autowired
+
     private PrivilegeRepository privRepo;
-    @Autowired
+
     private RoleRepository roleRepo;
-    @Autowired
+
     private GroupRepository groupRepo;
+
+    @Autowired
+    public void setPrivRepo(PrivilegeRepository privRepo) {
+        this.privRepo = privRepo;
+    }
+
+    @Autowired
+    public void setRoleRepo(RoleRepository roleRepo) {
+        this.roleRepo = roleRepo;
+    }
+
+    @Autowired
+    public void setGroupRepo(GroupRepository groupRepo) {
+        this.groupRepo = groupRepo;
+    }
 
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent event) {
-
-        Map<String, List> RPmap = new LinkedHashMap<>();
-        List<String> aPriv = new ArrayList<String>();
-        List<String> rPriv = Arrays.asList(Privileges.values()[0].toString(), Privileges.values()[3].toString(), Privileges.values()[6].toString(), Privileges.values()[8].toString(), Privileges.values()[12].toString(), Privileges.values()[14].toString(), Privileges.values()[18].toString());
-        List<String> wPriv = Arrays.asList(Privileges.values()[0].toString(), Privileges.values()[1].toString(), Privileges.values()[3].toString(), Privileges.values()[4].toString(), Privileges.values()[6].toString(), Privileges.values()[7].toString(), Privileges.values()[8].toString(), Privileges.values()[9].toString(), Privileges.values()[12].toString(), Privileges.values()[13].toString(), Privileges.values()[14].toString(), Privileges.values()[15].toString(), Privileges.values()[18].toString());
-
-        for (int i = 0; i < Privileges.values().length; i++) {
-            aPriv.add(Privileges.values()[i].toString());
-        }
-
-        RPmap.put(Roles.ADMIN.toString(), aPriv);
-        RPmap.put(Roles.READER.toString(), rPriv);
-        RPmap.put(Roles.WRITER.toString(), wPriv);
 
         for (String role : RPmap.keySet()) {
             Optional<Role> roleIn = roleRepo.findByName(role);
