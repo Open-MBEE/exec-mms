b1b216fa833cda28691649da6427edb221b6f5c1
Lam, Doris T (319E) <Doris.T.Lam@jpl.nasa.gov>
2020-09-23 15:13:41 -0700

    separate configs

--- localuser/src/main/java/org/openmbee/sdvc/localuser/config/LocalUserSecurityConfig.java
+++ localuser/src/main/java/org/openmbee/sdvc/localuser/config/LocalUserSecurityConfig.java
@@ -1,67 +1,28 @@
 package org.openmbee.sdvc.localuser.config;
 ;
-import org.openmbee.sdvc.localuser.security.UserDetailsServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class LocalUserSecurityConfig {
 
     private static Logger logger = LoggerFactory.getLogger(LocalUserSecurityConfig.class);
+
+    private DaoAuthenticationProvider daoAuthenticationProvider;
+
     @Autowired
-    public UserDetailsServiceImpl userDetailsService;
-    @Value("${sdvc.admin.username}")
-    private String adminUsername;
-    @Value("${sdvc.admin.password}")
-    private String adminPassword;
-
-    public LocalUserSecurityConfig() {
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(){
-            //Turn off warnings for null/empty passwords
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                if (encodedPassword == null || encodedPassword.length() == 0) {
-                    return false;
-                }
-                return super.matches(rawPassword, encodedPassword);
-            }
-        };
+    public void setDaoAuthenticationProvider(DaoAuthenticationProvider daoAuthenticationProvider) {
+        this.daoAuthenticationProvider = daoAuthenticationProvider;
     }
 
     @Autowired
     public void configureDaoAuth(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationProvider());
+        auth.authenticationProvider(daoAuthenticationProvider);
     }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        try {
-            userDetailsService.loadUserByUsername(adminUsername);
-        } catch (UsernameNotFoundException e) {
-            userDetailsService.register(adminUsername, adminPassword, true);
-            logger.info(String.format("Creating root user: %s with specified password.",
-                adminUsername));
-        }
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-
 }
