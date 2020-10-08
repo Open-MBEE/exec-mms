1bf946699b2407851d84dc84e50b319816c87f2d
Lam, Doris T (319E) <Doris.T.Lam@jpl.nasa.gov>
2020-10-08 11:35:25 -0700

    use an auth provider and auth manager to authenticate so it hooks in to publishing auditing events if actuator is present

--- authenticator/src/main/java/org/openmbee/sdvc/authenticator/config/AuthSecurityConfig.java
+++ authenticator/src/main/java/org/openmbee/sdvc/authenticator/config/AuthSecurityConfig.java
@@ -1,28 +1,27 @@
 package org.openmbee.sdvc.authenticator.config;
 
-
 import org.openmbee.sdvc.authenticator.security.JwtAuthenticationEntryPoint;
+import org.openmbee.sdvc.authenticator.security.JwtAuthenticationProvider;
 import org.openmbee.sdvc.authenticator.security.JwtAuthenticationTokenFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableWebSecurity
-@EnableTransactionManagement
 public class AuthSecurityConfig {
 
     private static Logger logger = LoggerFactory.getLogger(AuthSecurityConfig.class);
 
-    public AuthSecurityConfig() {
+    @Autowired
+    public void setAuthProvider(AuthenticationManagerBuilder auth,
+            JwtAuthenticationProvider provider) {
+        auth.authenticationProvider(provider);
     }
 
     public void setAuthConfig(HttpSecurity http) throws Exception {
@@ -34,7 +33,7 @@ public class AuthSecurityConfig {
     }
 
     @Bean
-    public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter() throws Exception {
+    public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter() {
         return new JwtAuthenticationTokenFilter();
     }
 }
