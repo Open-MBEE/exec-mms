9c814c7f884e43be24bb86cc8351accc0561f57b
Jason Han <jason.han@jpl.nasa.gov>
2021-02-09 09:41:52 -0800

    Clean up runtime exceptions

--- core/src/main/java/org/openmbee/mms/core/dao/NodeIndexDAO.java
+++ core/src/main/java/org/openmbee/mms/core/dao/NodeIndexDAO.java
@@ -5,23 +5,22 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import org.openmbee.mms.core.exceptions.MMSException;
 import org.openmbee.mms.json.BaseJson;
 import org.openmbee.mms.json.ElementJson;
 
 public interface NodeIndexDAO {
 
-    void indexAll(Collection<? extends BaseJson> jsons) throws MMSException;
+    void indexAll(Collection<? extends BaseJson> jsons);
 
-    void index(BaseJson json) throws MMSException;
+    void index(BaseJson json);
 
     Optional<ElementJson> findById(String docId);
 
     List<ElementJson> findAllById(Set<String> docIds);
 
-    void deleteById(String docId) throws MMSException;
+    void deleteById(String docId);
 
-    void deleteAll(Collection<? extends BaseJson> jsons) throws MMSException;
+    void deleteAll(Collection<? extends BaseJson> jsons);
 
     boolean existsById(String docId);
 
