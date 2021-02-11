be896064184ce116c9cfd12d12720c7463bab5d2
Ward Schweitzer <ward.a.schweitzer@boeing.com>
2021-02-11 12:23:57 -0500

    Adding groups api

---
+++ groups/src/main/java/org/openmbee/mms/groups/constants/GroupConstants.java
@@ -0,0 +1,14 @@
+package org.openmbee.mms.groups.constants;
+
+public class GroupConstants {
+
+    public static final String GROUP_ALREADY_EXISTS = "Group already exists";
+    public static final String GROUP_NOT_EMPTY = "Group is not empty";
+    public static final String GROUP_NOT_FOUND = "Group not found";
+    public static final String INVALID_ACTION = "Invalid action";
+    public static final String INVALID_GROUP_NAME= "Invalid group name";
+    public static final String NAME = "name";
+    public static final String NO_USERS_PROVIDED = "No users provided";
+    public static final String RESTRICTED_GROUP = "Restricted group";
+
+}
