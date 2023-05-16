02759b40f864e811b48dddd42029d94545d44038
Doris Lam <dlam@jpl.nasa.gov>
2023-05-16 09:23:09 -0700

    compatibility with cameo 2022 stereotype ids key

--- cameo/src/main/java/org/openmbee/mms/cameo/services/CameoHelper.java
+++ cameo/src/main/java/org/openmbee/mms/cameo/services/CameoHelper.java
@@ -49,6 +49,9 @@ public class CameoHelper {
 
     public boolean isView(ElementJson e) {
         List<String> sids = (List)e.get(CameoConstants.APPLIEDSTEREOTYPEIDS);
+        if (sids == null) {
+            sids = (List)e.get(CameoConstants.OLDAPPLIEDSTEREOTYPEIDS);
+        }
         if (sids != null && !sids.isEmpty()) {
             Set<String> ids = new HashSet<>(sids);
             ids.retainAll(CameoConstants.VIEWSIDS);
@@ -59,6 +62,9 @@ public class CameoHelper {
 
     public boolean isDocument(ElementJson e) {
         List<String> sids = (List)e.get(CameoConstants.APPLIEDSTEREOTYPEIDS);
+        if (sids == null) {
+            sids = (List)e.get(CameoConstants.OLDAPPLIEDSTEREOTYPEIDS);
+        }
         if (sids != null && sids.contains(CameoConstants.DOCUMENTSID)) {
             return true;
         }
@@ -85,10 +91,10 @@ public class CameoHelper {
         res.put(CameoConstants.ASSOCIATIONID, assocId);
         List<String> asIds = new ArrayList<>();
         res.put(CameoConstants.APPLIEDSTEREOTYPEIDS, asIds);
+        res.put(CameoConstants.OLDAPPLIEDSTEREOTYPEIDS, asIds);
         res.put(CameoConstants.DOCUMENTATION, "");
         res.put(CameoConstants.MDEXTENSIONSIDS, new ArrayList());
         res.put(CameoConstants.SYNCELEMENTID, null);
-        res.put(CameoConstants.APPLIEDSTEREOTYPEINSTANCEID, null);
         res.put(CameoConstants.CLIENTDEPENDENCYIDS, new ArrayList());
         res.put(CameoConstants.SUPPLIERDEPENDENCYIDS, new ArrayList());
         res.put(CameoConstants.VISIBILITY, "private");
@@ -135,7 +141,6 @@ public class CameoHelper {
         association.put(CameoConstants.MDEXTENSIONSIDS, new ArrayList());
         association.put(CameoConstants.SYNCELEMENTID, null);
         association.put(CameoConstants.APPLIEDSTEREOTYPEIDS, new ArrayList());
-        association.put(CameoConstants.APPLIEDSTEREOTYPEINSTANCEID, null);
         association.put(CameoConstants.CLIENTDEPENDENCYIDS, new ArrayList());
         association.put(CameoConstants.SUPPLIERDEPENDENCYIDS, new ArrayList());
         association.put(CameoConstants.NAMEEXPRESSION, null);
