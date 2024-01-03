eb17e9a9d91445854f020b1a0191306217583840
chas galey <chas.galey@sncorp.com>
2024-01-03 09:31:34 -0700

    Propose Elastic Authentication capabilities

--- elastic/src/main/java/org/openmbee/mms/elastic/config/ElasticsearchConfig.java
+++ elastic/src/main/java/org/openmbee/mms/elastic/config/ElasticsearchConfig.java
@@ -1,8 +1,14 @@
 package org.openmbee.mms.elastic.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,13 +24,36 @@ public class ElasticsearchConfig {
     @Value("${elasticsearch.http}")
     private String elasticsearchHttp;
 
+    @Value("${elasticsearch.password}")
+    private String elasticsearchPassword;
+    @Value("${elasticsearch.username}")
+    private String elasticsearchUsername;
+
     @Bean(name = "clientElastic", destroyMethod = "close")
     public RestHighLevelClient restClient() {
 
+        
+
         RestClientBuilder builder = RestClient.builder(new HttpHost(elasticsearchHost, elasticsearchPort, elasticsearchHttp));
         builder.setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setConnectTimeout(10000).setSocketTimeout(1000000));
-        RestHighLevelClient client = new RestHighLevelClient(builder);
-        return client;
+
+        if (! elasticsearchPassword.isEmpty() && ! elasticsearchUsername.isEmpty()) {
+            final CredentialsProvider credentialsProvider =
+                new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(elasticsearchUsername, elasticsearchPassword));
+            builder.setHttpClientConfigCallback(new HttpClientConfigCallback() {
+            @Override
+            public HttpAsyncClientBuilder customizeHttpClient(
+                    HttpAsyncClientBuilder httpClientBuilder) {
+                return httpClientBuilder
+                    .setDefaultCredentialsProvider(credentialsProvider);
+            }
+        });
+        }
+        
+
+        return new RestHighLevelClient(builder);
     }
 
 }
